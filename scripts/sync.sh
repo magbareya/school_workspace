4ea18e18f4793858735a82da2f7045825a0cbe9b
Mahmoud Agbareya <mahmoud.agbareya@gmail.com>
2025-10-05 17:15:00 +0300

    Add HW2 for basics

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,1 +1,4 @@
-scp -r out/* "C:\Users\magbareya\OneDrive\ملفاتي\مدرسة ثانوية\fromWorkspace"
+make clean -j
+make all -j
+make sclean -j
+scp -r out/* "C:\Users\magbareya\OneDrive\ملفاتي\مدرسة ثانوية\موادي"
